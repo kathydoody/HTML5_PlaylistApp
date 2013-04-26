633bf7abd079a36b2fc58b160e69da613041cea4
Simon Bailey <simon@newtriks.com>
2013-04-26 16:23:14 +0100

    kvList test
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- test/spec/directives/kvList.js
+++ test/spec/directives/kvList.js
@@ -1,13 +1,44 @@
 'use strict';
 
-describe('Directive: kvList', function () {
-  beforeEach(module('knowledgevisionHtml5PlaylistappApp'));
+describe('Directive: kvList', function() {
+	var element, scope, $rootScope, $compile;
 
-  var element;
+	beforeEach(angular.mock.module('knowledgevisionHtml5PlaylistappApp'));
 
-  it('should make hidden element visible', inject(function ($rootScope, $compile) {
-    element = angular.element('<kv-list></kv-list>');
-    element = $compile(element)($rootScope);
-    expect(element.text()).toBe('this is the kvList directive');
-  }));
+	beforeEach(inject(
+	['$compile', '$rootScope', function($c, $r) {
+		$compile = $c;
+		$rootScope = $r;
+	}]));
+	//beforeEach(module('views/templates/videoList.html'));
+	var $httpBackend;
+
+	beforeEach(inject(function($injector) {
+		$httpBackend = $injector.get('$httpBackend');
+		$httpBackend.whenGET('views/templates/videoList.html').passThrough();
+	}));
+
+	// beforeEach(inject(function($rootScope, $compile) {
+	// 	// we might move this tpl into an html file as well...
+	// 	scope = $rootScope;
+	// 	scope.videos = [{
+	// 		'id': '1',
+	// 		'type': 'mp4',
+	// 		'title': 'Big Buck Bunny',
+	// 		'src': 'assets/big_buck_bunny.mp4',
+	// 		'startTime': 14
+	// 	}];
+	// 	scope.selectedVideo = scope.videos[0];
+	// 	scope.videoTime = 0;
+	// 	element = angular.element('<video-link ng-repeat="vid in videos" id="{{vid.id}}" type="{{vid.type}}" title="{{vid.title}}" src="{{vid.src}}" time="{{vid.startTime}}" index="{{$index + 1}}"></video-link>');
+	// 	$compile(element)(scope);
+	// 	scope.$digest();
+	// }));
+
+	it("should display the welcome text properly", function() {
+		var element = $compile('<video-link ng-repeat="vid in videos" id="{{vid.id}}" type="{{vid.type}}" title="{{vid.title}}" src="{{vid.src}}" time="{{vid.startTime}}" index="{{$index + 1}}"></video-link>')($rootScope);
+
+		//var element = $compile('<div data-app-welcome>User</div>')($rootScope);
+		expect(element.html()).to.match(/Welcome/i);
+	})
 });
