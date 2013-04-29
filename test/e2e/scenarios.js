2fdeed01e61179aeb3eb0eb88fb15858ab43e857
Simon Bailey <simon@newtriks.com>
2013-04-29 13:39:52 +0100

    Maybe a solution?
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,14 +1,20 @@
 'use strict';
 
 describe('knowledgevisionHtml5PlaylistappApp App', function() {
-
+	angular.scenario.dsl('value', function() {
+		return function(value) {
+			return this.addFuture('value to future', function(done) {
+				done(null, value);
+			});
+		};
+	});
 	// http://localhost:9000/#/
 	beforeEach(function() {
-      browser().navigateTo('/__e2e');
-    });
+		browser().navigateTo('/__e2e');
+	});
 
 	it('should fail', function() {
-		expect(true).toBe(true);
+		expect(value(42)).toBe(42);
 	});
 
 });
